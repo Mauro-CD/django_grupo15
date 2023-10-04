15c26170aab4f713639474e44ded332ce2f14ec8
Mauro Caceres D'Amore <mauro.ocd@gmail.com>
2023-10-04 01:11:16 -0300

    conexion con postgres

--- elearning/settings.py
+++ elearning/settings.py
@@ -82,13 +82,23 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "grupo15",
+        "USER": "postgres",
+        "PASSWORD": "django2023",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
