703e834fe86ad092f6c5410a3c30880d330f47f3
Mauro Caceres D'Amore <mauro.ocd@gmail.com>
2023-10-19 20:41:15 -0300

    ofuscacion de claves

--- elearning/settings.py
+++ elearning/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-((z^=t)ag4dk6x^pif!$(7qt-4yg59=5z8cnbp#fwy=1*v@3l('
+# SECRET_KEY = 'django-insecure-((z^=t)ag4dk6x^pif!$(7qt-4yg59=5z8cnbp#fwy=1*v@3l('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -92,11 +94,11 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "grupo15",
-        "USER": "postgres",
-        "PASSWORD": "django2023",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "NAME": config('NAME'),
+        "USER": config('USER'),
+        "PASSWORD": config('PASSWORD'),
+        "HOST": config('HOST'),
+        "PORT": config('PORT'),
     }
 }
 
