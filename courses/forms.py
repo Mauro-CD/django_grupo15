8ad2d64b896cd597c46ea4d39715f53ce6915437
Mauro Caceres D'Amore <mauro.ocd@gmail.com>
2023-10-24 19:05:33 -0300

    generacion de modelos

--- courses/forms.py
+++ courses/forms.py
@@ -2,38 +2,41 @@
 
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User #agregado 22 octubre
 
 
 class CourseFilterForm(forms.Form):
     search = forms.CharField(label='Search', required=False)
     # Add more filter fields as needed
 
-class Estilos(forms.TextInput):
-    CSS = {'all': ('red_estilos.css')}
+# class Estilos(forms.TextInput):
+#     CSS = {'all': ('red_estilos.css')}
 
-class EstilosInput():
-    attrs={'class': 'formInput'}
+# class EstilosInput():
+#     attrs={'class': 'formInput'}
 
-class RegistrarForm(forms.Form):
-    user = forms.CharField(label='Usuario',  required=True)
-    nombre = forms.CharField(label='Nombre',  required=True)
-    apellido = forms.CharField(label='Apellido:',  required=True)
-    email = forms.EmailField(label='Correo Electrónico',  required=True)
-    password = forms.CharField(label='Contraseña:', widget=forms.PasswordInput,   required=True)
-    passwordConfirm = forms.CharField(label='Confirmar Contraseña:', widget=forms.PasswordInput,  required=True)
+# class RegistrarForm(forms.Form):
+#     user = forms.CharField(label='Usuario',  required=True)
+    # nombre = forms.CharField(label='Nombre',  required=True)
+    # apellido = forms.CharField(label='Apellido:',  required=True)
+    # email = forms.EmailField(label='Correo Electrónico',  required=True)
+    # password = forms.CharField(label='Contraseña:', widget=forms.PasswordInput,   required=True)
+    # passwordConfirm = forms.CharField(label='Confirmar Contraseña:', widget=forms.PasswordInput,  required=True)
 
 
-    def clean_user(self):
-        if self.cleaned_data['user'] == "carlos":
-            raise ValidationError("El usuario ya existe")
-        return self.cleaned_data['user']
+    # def clean_user(self):
+    #     if self.cleaned_data['user'] == "carlos":
+    #         raise ValidationError("El usuario ya existe")
+    #     return self.cleaned_data['user']
     
-    def clean(self):
-        if self.cleaned_data['password'] !=  self.cleaned_data['passwordConfirm']:
-             print("clave incorrecta")
-             raise ValidationError("La contraseña no coincide")
-        return self.cleaned_data
+    # def clean(self):
+    #     if self.cleaned_data['password'] !=  self.cleaned_data['passwordConfirm']:
+    #          print("clave incorrecta")
+    #          raise ValidationError("La contraseña no coincide")
+    #     return self.cleaned_data
 
+
+################################################# Usuario ###################################################################
 #agregado 22 octubre, actualmente este registra el usuario y lo envìa a la tabla default del db (auth_user)
 class UserRegistrationForm(forms.Form):
     name = forms.CharField(label='Nombre', min_length=2)
@@ -41,10 +44,52 @@ class UserRegistrationForm(forms.Form):
     email = forms.EmailField(label='Correo Electrónico')
     password = forms.CharField(label='Contraseña', widget=forms.PasswordInput, min_length=6)
     password2 = forms.CharField(label='Confirmar Contraseña', widget=forms.PasswordInput, min_length=6)
+    
+    # def clean_email(self):
+    #     if self.cleaned_data['email'] == "email":
+    #         raise ValidationError("El usuario ya existe")
+    #     return self.cleaned_data['email']
+    
+    def clean(self):
+        if self.cleaned_data['password'] !=  self.cleaned_data['password2']:
+             print("clave incorrecta")
+             raise ValidationError("La contraseña no coincide")
+        return self.cleaned_data
 
 
+
+################################################# CONTACTO ###################################################################
 class ContactoForm(forms.Form):
     nombre = forms.CharField(label="Nombre de contacto",   widget=forms.TextInput(attrs={'class': 'formulario','placeholder': 'Solo letras'}  ),required=True)
     email = forms.EmailField(label='E-mail',max_length=100,required=True,error_messages={'required': 'Por favor completa el campo'},widget=forms.TextInput(attrs={'class': 'formulario','placeholder': 'ejemplo@email.com'}))
-    telefono =  forms.IntegerField(label="Teléfono",widget=forms.TextInput(attrs={'class': 'formulario','placeholder': 'Solo números'}  ), min_value=0)
+    telefono = forms.IntegerField(label="Teléfono",widget=forms.TextInput(attrs={'class': 'formulario','placeholder': 'Solo números'}  ), min_value=0)
     mensaje = forms.CharField(widget=forms.Textarea)
+
+
+################################################# Foro ###################################################################
+class ForoForm(forms.Form):
+    # usuario = forms.CharField(label="Usuario",   widget=forms.TextInput(attrs={'class': 'formulario','placeholder': 'Solo letras'}  ),required=True)
+    titulo = forms.CharField(label="Usuario",   widget=forms.TextInput(attrs={'class': 'formulario','placeholder': 'Solo letras'}  ),required=True)
+    fecha = forms.DateField(label="Fecha")
+    contenido = forms.CharField(widget=forms.Textarea)
+
+
+################################################# Direccion ###################################################################
+class DireccionForm(forms.Form):
+    calle = forms.CharField(label="Usuario",   widget=forms.TextInput(attrs={'class': 'formulario','placeholder': 'Solo letras'}  ),required=True)
+    altura = forms.IntegerField(label="altura",   widget=forms.TextInput(attrs={'class': 'formulario','placeholder': 'Solo letras'}  ),required=True, max_value=100000)
+    ciudad = forms.CharField(label="Usuario",   widget=forms.TextInput(attrs={'class': 'formulario','placeholder': 'Solo letras'}  ),required=True)
+    pais = forms.CharField(label="Usuario",   widget=forms.TextInput(attrs={'class': 'formulario','placeholder': 'Solo letras'}  ),required=True)
+
+
+################################################# Estudiante ###################################################################
+
+
+
+################################################# Docente ################################################################### 
+
+
+################################################# Curso ################################################################### 
+
+
+################################################# CursoInscripto ################################################################### 
--- courses/models.py
+++ courses/models.py
@@ -1,11 +1,34 @@
 # courses/models.py
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import User #agregado 22 octubre
 
+
+class Docente(User):
+    legajo = models.IntegerField()
+
+#### relacion uno a mucho: Docente -> Course 
 class Course(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    titulo = models.CharField(max_length=255)
+    duracion = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    docente = models.ForeignKey(Docente, on_delete=models.CASCADE)
+
+
+#### relacion mucho a mucho mediante inscripcion: Estudiante -> CursoInscripto 
+class CursoInscripto(Course):
+    habilitado =  bool()
+
+class Estudiante(User):
+    matricula = models.IntegerField()
+    Activo = bool()
+    cursoInscripto = models.ManyToManyField(CursoInscripto, through="inscripcion")
+
+class inscripcion(models.Model):
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    curso = models.ForeignKey(CursoInscripto, on_delete=models.CASCADE)
+    
 
 class UserCourse(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -24,3 +47,20 @@ class ContactMessage(models.Model):
     email = models.EmailField(max_length=100)
     telefono = models.CharField(max_length=20)
     mensaje = models.TextField()
+
+#### relacion uno a mucho: User -> Foro
+class Foro(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    fecha = models.DateField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+#### relacion uno a uno: User -> Direccion
+class Direccion(models.Model):
+    calle = models.CharField(max_length=100)
+    altura = models.IntegerField()
+    ciudad = models.CharField(max_length=100)
+    pais = models.CharField(max_length=100)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+ 
+
