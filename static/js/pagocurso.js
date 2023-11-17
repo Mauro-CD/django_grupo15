72503cc2352a371957c5af3c3accb506871cd355
gpascu <pascuzzige@gmail.com>
2023-11-17 23:30:00 +0100

    Modificaciòn funcione js_fecha

--- static/js/pagocurso.js
+++ static/js/pagocurso.js
@@ -10,9 +10,9 @@ function confirmPayment() {
     var cvv = document.getElementById("cvv").value;
     var nombre = document.getElementById("nombreUsuario").innerText;
     var apellido = document.getElementById("apellidoUsuario").innerText;
-    var id = document.getElementById("idUsuario").innerText;
-    var email = document.getElementById("emailUsuario").innerText;
-
+    var id = parseInt(document.getElementById("idUsuario").innerText);
+    var email = document.getElementById("emailUsuario").innerText; 
+    var matricula = parseInt(id) + 5000;
     // Ejecutar validación
     if (creditCardNumber.length !== 16 || !expiryDate.match(/^(0[1-9]|1[0-2])\/[0-9]{4}$/) || cvv.length !== 3) {
         document.getElementById("paymentMessage").innerText = "Información de pago inválida. Verifica los datos ingresados.";
@@ -22,9 +22,33 @@ function confirmPayment() {
     // Mensaje de pago (simulación)
     document.getElementById("paymentMessage").innerText = "Pago realizado con éxito. ¡Gracias por tu compra!";
 
+    // Componentes de la fecha en formato string simple
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1; 
+    const day = currentDate.getDate();
+    //componentes fecha en formato mas detallado
+    var currentDate2 = new Date();
+    // Formateo fecha en el formato del db
+    var formattedDate =
+    currentDate.getFullYear() +
+    "-" +
+    ((currentDate.getMonth() + 1) < 10 ? '0' : '') + (currentDate.getMonth() + 1) +
+    "-" +
+    (currentDate.getDate() < 10 ? '0' : '') + currentDate.getDate() +
+    " " +
+    (currentDate.getHours() < 10 ? '0' : '') + currentDate.getHours() +
+    ":" +
+    (currentDate.getMinutes() < 10 ? '0' : '') + currentDate.getMinutes() +
+    ":" +
+    (currentDate.getSeconds() < 10 ? '0' : '') + currentDate.getSeconds() +
+    "." +
+    (currentDate.getMilliseconds() < 10 ? '00' : (currentDate.getMilliseconds() < 100 ? '0' : '')) + currentDate.getMilliseconds() +
+    "000" + 
+    "+00";
     // Obtener parámetros de la URL
     var urlParams = new URLSearchParams(window.location.search);
-    var cursoId = urlParams.get('curso_id');
+    var cursoId = parseInt(urlParams.get('curso_id'));
     var cursoTitulo = urlParams.get('curso_titulo');
     var cursoDocenteId = urlParams.get('curso_docente_id');
 
@@ -38,28 +62,10 @@ function confirmPayment() {
         console.log('Apellido:',apellido);
         console.log('Id:',id);
         console.log('Email:',email);
+        console.log('Matricula:',matricula);
+        console.log(`Current Date: ${day}/${month}/${year}`);
+        console.log("fecha en formato",formattedDate);
         // También puedes mostrar un mensaje en la consola
         console.log('Compra confirmada para el curso:', cursoTitulo);
     }
 }
-
-
-// pagocurso.js
-/*
-
-document.addEventListener('DOMContentLoaded', function () {
-    var urlParams = new URLSearchParams(window.location.search);
-    var cursoId = urlParams.get('curso_id');
-    var cursoTitulo = urlParams.get('curso_titulo');
-    var cursoDocenteId = urlParams.get('curso_docente_id');
-
-    // Check if the necessary parameters are present
-    if (cursoId && cursoTitulo && cursoDocenteId) {
-        console.log('Curso ID:', cursoId);
-        console.log('Curso Titulo:', cursoTitulo);
-        console.log('Curso Docente ID:', cursoDocenteId);
-
-        // Display an alert with the information
-        alert('Compra confirmada para el curso:\n\nID: ' + cursoId + '\nTitulo: ' + cursoTitulo + '\nDocente ID: ' + cursoDocenteId);
-    }
-});*/
